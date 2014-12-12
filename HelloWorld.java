85bed75065b8272b332b05dc6c46a239e15d86e3
unknown <QV47@INLTE6749.in.tesco.org>
2014-12-12 18:15:32 +0530

    Added .

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
   public static void main(String[] args){
     System.out.println("Hello World!");
-    System.out.println("Hey Man.:");
+    System.out.println("Hey Man.:how are you doing??");
+	for(int i=0;i++;i<4)
+	{System.out.println(i);
+	}
+
   }
 }
